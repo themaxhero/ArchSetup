25b7c219315520b159a5df9872a488572ef7c563
Marcelo Amancio de Lima Santos <contact@maxhero.dev>
2025-06-04 09:50:06 -0300

    use envvar to create BTRFS

--- 00-prepare.sh
+++ 00-prepare.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
 # Partitioning
-export DEVICE="/dev/sda"
-mkfs.btrfs -f $DEVICE
-mount /dev/sdX /mnt
+cryptsetup -v luksFormat $DEVICE
+cryptsetup open $DEVICE root
+
+export BTRFS_DEVICE="/dev/mapper/root"
+mkfs.btrfs -f $BTRFS_DEVICE
+mount $BTRFS_DEVICE /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@etc
 btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@opt
 btrfs subvolume create /mnt/@snapshots
 btrfs subvolume create /mnt/@nix
 
 umount /mnt
 
-mount -o noatime,compress=zstd,subvol=@ $DEVICE /mnt
-mkdir -p /mnt/{home,etc,nix,var,.snapshots}
-mount -o noatime,compress=zstd,subvol=@home $DEVICE /mnt/home
-mount -o noatime,compress=zstd,subvol=@etc $DEVICE /mnt/etc
-mount -o noatime,compress=zstd,subvol=@var $DEVICE /mnt/var
-mount -o noatime,compress=zstd,subvol=@nix $DEVICE /mnt/nix
-mount -o noatime,compress=zstd,subvol=@snapshots $DEVICE /mnt/.snapshots
+mount -o noatime,compress=zstd,subvol=@ $BTRFS_DEVICE /mnt
+mkdir -p /mnt/{home,etc,opt,nix,var,.snapshots}
+mount -o noatime,compress=zstd,subvol=@home $BTRFS_DEVICE /mnt/home
+mount -o noatime,compress=zstd,subvol=@etc $BTRFS_DEVICE /mnt/etc
+mount -o noatime,compress=zstd,subvol=@var $BTRFS_DEVICE /mnt/var
+mount -o noatime,compress=zstd,subvol=@nix $BTRFS_DEVICE /mnt/nix
+mount -o noatime,compress=zstd,subvol=@opt $BTRFS_DEVICE /mnt/opt
+mount -o noatime,compress=zstd,subvol=@snapshots $BTRFS_DEVICE /mnt/.snapshots
 
 # Bootstrap System inside the /mnt
 pacstrap -K /mnt base base-devel linux linux-firmware
